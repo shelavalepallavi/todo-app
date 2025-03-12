77c3458f77cadc5f352746e3fb85becef9b2cdbd
shelavale3795 <pallavishelavale3795@gmail.com>
2025-03-12 13:22:23 +0530

    change in code

--- src/redux/weatherActions.js
+++ src/redux/weatherActions.js
@@ -1,23 +1,52 @@
+// export const fetchWeather = (city) => async (dispatch) => {
+//   if (!city.trim()) return;
+//   dispatch({ type: "FETCH_WEATHER_REQUEST" });
+
+//   try {
+//     const apiKey = process.env.REACT_APP_API_KEY;
+//     const res = await fetch(
+//       `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+//     );
+//     const data = await res.json();
+
+//     if (data.cod === 200) {
+//       dispatch({
+//         type: "FETCH_WEATHER_SUCCESS",
+//         payload: { temp: data.main.temp, description: data.weather[0].description },
+//       });
+//     } else {
+//       dispatch({ type: "FETCH_WEATHER_FAILURE", payload: "City not found" });
+//     }
+//   } catch (error) {
+//     dispatch({ type: "FETCH_WEATHER_FAILURE", payload: "API Error" });
+//   }
+// };
 export const fetchWeather = (city) => async (dispatch) => {
   if (!city.trim()) return;
   dispatch({ type: "FETCH_WEATHER_REQUEST" });
 
   try {
-    const apiKey = "4e4a4ddbd8e3ce2bddae06b174d557a0 ";
+    const apiKey = import.meta.env.VITE_API_KEY;
     const res = await fetch(
       `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
     );
-    const data = await res.json();
 
-    if (data.cod === 200) {
-      dispatch({
-        type: "FETCH_WEATHER_SUCCESS",
-        payload: { temp: data.main.temp, description: data.weather[0].description },
-      });
-    } else {
-      dispatch({ type: "FETCH_WEATHER_FAILURE", payload: "City not found" });
+    if (!res.ok) {
+      throw new Error(`Error: ${res.status} - ${res.statusText}`);
     }
+
+    const data = await res.json();
+    dispatch({
+      type: "FETCH_WEATHER_SUCCESS",
+      payload: {
+        temp: data.main.temp,
+        description: data.weather[0].description,
+      },
+    });
   } catch (error) {
-    dispatch({ type: "FETCH_WEATHER_FAILURE", payload: "API Error" });
+    dispatch({
+      type: "FETCH_WEATHER_FAILURE",
+      payload: error.message || "API Error",
+    });
   }
 };
